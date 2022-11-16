0aad019f47a48b507577662efecddfebd0f518b2
Jan Schuetz <janalexschuetz@gmail.com>
2022-11-16 14:14:00 +1100

    Implemented backend for watch list

---
+++ app/models/bookmark.rb
@@ -0,0 +1,7 @@
+class Bookmark < ApplicationRecord
+  belongs_to :list
+  belongs_to :movie
+
+  validates_uniqueness_of :list_id, scope: :movie_id
+  validates :comment, length: { minimum: 6 }
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  get 'lists/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  delete '/bookmarks/:id', to: 'bookmarks#destroy', as: 'delete_bookmark'
+
+  resources :lists, only: %i[index show new create] do
+    resources :bookmarks, only: %i[new create]
+  end
 end
